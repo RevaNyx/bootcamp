98419c62729e30853d4a6517c6b7f3637b9e2231
Tamir Harris <tamirharris@Tamirs-MacBook-Air.local>
2024-10-29 13:29:25 -0500

    loops-and-iterators assignment

--- ruby/classwork/loops-and-iterators/script.rb
+++ ruby/classwork/loops-and-iterators/script.rb
@@ -1,30 +1,32 @@
 #1.
 =begin
 Set the value of the variable num to 1.
-While the variable num is less than or equal to 20, increment by one. 
+While the variable num is less than or equal to 20.. 
 If the value of num is even, print 'Even. ' Otherwise print 'Odd. '
+Increment by one.
 =end
 num = 1
 while num <= 20
-    num = num + 1
+    
     if num.even?
-        print 'Even. '
+        print "#{num} Even. "
     else
-        print 'Odd. '
+        print "#{num} Odd. "
     end
+    num = num + 1
 end
 
 #2.
 =begin
 Set the value of the variable num to 1. 
-From values 1-20, if num is even, print 'Even. '. 
+For num values 1-20, if num is even, print 'Even. '. 
 Otherwise print 'Odd. '
 =end
 num = 1
 for num in 1..20
     if num.even?
-        print 'Even. '
+        print "#{num} Even. "
     else
-        print 'Odd. '
+        print "#{num} Odd. "
     end
 end
