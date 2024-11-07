fbabc54a450f73834006c0c929861901b9c17490
Tamir Harris <tamirharris@Tamirs-MacBook-Air.local>
2024-11-07 12:17:07 -0600

    update w/ lesson 8

--- ruby/homework/blog/config/routes.rb
+++ ruby/homework/blog/config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :articles
+  resources :articles do 
+  resources :comments
+  end
+
 end
