4e9475c6e275949c5583cf833435d19ffcbc7a33
Tamir Harris <tamirharris@Tamirs-MacBook-Air.local>
2024-11-07 14:00:25 -0600

    added categories

--- ruby/homework/blog/app/models/article.rb
+++ ruby/homework/blog/app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
+    belongs_to :category
     has_many :comments
-    
+
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
   end
--- ruby/homework/blog/config/routes.rb
+++ ruby/homework/blog/config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :categories
+  
   resources :articles do 
   resources :comments
   end
